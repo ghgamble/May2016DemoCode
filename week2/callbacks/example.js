c289dde7e568de391cd7cccea945b96fae51690b
jiminikiz <brandon@augur.io>
2016-05-26 10:05:50 -0600

    Updating notes to include missing example

--- week2/callbacks/example.js
+++ week2/callbacks/example.js
@@ -11,7 +11,20 @@ function nameJoiner( firstName, lastName ) {
 
 Namer('Tony','Montana', nameJoiner);
 
-// var intervalId setInterval(callback, delay, [parameters...]);
+// var intervalId = setInterval(callback, delay, [parameters...]);
+
+var count = 0;
+
+var intervalCounterId = setInterval(function(){
+    console.log(Date.now());
+
+    if( count === 3 ) {
+        clearInterval(intervalCounterId);
+    } else {
+        count++;
+    }
+}, 1000);
+
 
 var intervalId = setInterval(function(){
     console.log(Date.now());
@@ -22,8 +35,8 @@ var timeoutId = setTimeout(function(){
 }, 5000); // 5 sasdfeconds;
 
 for(var i=0; i<10; i++) {
-    // (function(j){
-    setTimeout(function(j){
+    // (function(j){ // using iffy
+    setTimeout(function(j){ // using setTimeout
         console.log(i);
     }, 1000*i, i);
     // })(i);

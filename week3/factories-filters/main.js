a1ac3076d188d7a44a52c73e7dd0091b9c31bf15
devaio <robert.camp@outlook.com>
2016-06-02 09:47:33 -0600

    Factories-Filters

--- week3/factories-filters/main.js
+++ week3/factories-filters/main.js
@@ -6,5 +6,51 @@ angular.module('SauceApp')
 
 function ketchupCtrl (){
 	console.log('hi from ketchup');
+	var ket = this;
+	ket.greeting = "Sugar and fake blood";
+	ket.menuItems = [
+		{
+			name : 'French Fries',
+			sauce : 'Ketchup',
+			mainIngredient : '\'Taters'
+		},
+		{
+			name : 'Dumplings',
+			sauce : 'SoySauce',
+			mainIngredient : 'Pigeon'
+		},
+		{
+			name : 'SoySauce Carrot stuff',
+			sauce : 'SoySauce',
+			mainIngredient : 'Carrots'
+		}
+	]
+}
+// =============Controller File ^^================== \\
+
+angular.module('SauceApp')
+	.controller('SoySauce', soysauceCtrl)
+
+function soysauceCtrl (){
+	console.log('hi from soysauce');
+	var soy = this;
+	soy.greeting = "Sodium and pigeons blood";
+	soy.menuItems = [
+		{
+			name : 'French Fries',
+			sauce : 'Ketchup',
+			mainIngredient : '\'Taters'
+		},
+		{
+			name : 'Dumplings',
+			sauce : 'SoySauce',
+			mainIngredient : 'Pigeon'
+		},
+		{
+			name : 'SoySauce Carrot stuff',
+			sauce : 'SoySauce',
+			mainIngredient : 'Carrots'
+		}
+	]
 }
 // =============Controller File ^^================== \\

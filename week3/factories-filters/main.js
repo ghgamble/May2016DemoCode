785e2924298cfd3d9a5487ed5d161be3fd0de2d2
devaio <robert.camp@outlook.com>
2016-06-02 09:30:55 -0600

    Factories-Filters Initial Controller

---
+++ week3/factories-filters/main.js
@@ -0,0 +1,10 @@
+angular.module('SauceApp', [])
+// ==============Module File ^^================= \\
+
+angular.module('SauceApp')
+	.controller('Ketchup', ketchupCtrl)
+
+function ketchupCtrl (){
+	console.log('hi from ketchup');
+}
+// =============Controller File ^^================== \\

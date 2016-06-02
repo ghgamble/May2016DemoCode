f983443ee7afe9c9c61c9eaeebbd5949a622bd16
devaio <robert.camp@outlook.com>
2016-06-02 11:38:06 -0600

    Factories-Filter final with filters

--- week3/factories-filters/main.js
+++ week3/factories-filters/main.js
@@ -25,7 +25,8 @@ function soysauceCtrl (SauceFactory, $timeout){
 	var soy = this;
 	soy.greeting = "Sodium and pigeons blood";
 	soy.menuItems = SauceFactory.data;
-
+	console.log('SOY', soy);
+	
 	function newItem(){
 		soy.menuItems.push({
 			name : 'TofuKetchup Medley',
@@ -37,6 +38,21 @@ function soysauceCtrl (SauceFactory, $timeout){
 
 	$timeout(newItem, 3000)
 
+	soy.cheapItems = function(element, index, array){
+		// console.log(arguments) // we don't know what angular is passing to this function!
+
+		// Filter expects a return of true (keep) or false(throw away)
+
+		return element.price < 5 ? true : false
+		// CONDITION ? TRUEVAL : FALSEVAL
+		// VS
+		// if(element.price < 5){
+		// 	return true
+		// }
+		// else{
+		// 	return false
+		// }
+	}
 }
 // =============Controller File ^^================== \\
 

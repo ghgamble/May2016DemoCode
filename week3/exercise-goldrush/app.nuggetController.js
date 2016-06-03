84b584ccc360c964f48232f80405b188707d3708
devaio <robert.camp@outlook.com>
2016-06-03 15:21:34 -0600

    ER: Gold-Rush : Making a marker from the event object

--- week3/exercise-goldrush/app.nuggetController.js
+++ week3/exercise-goldrush/app.nuggetController.js
@@ -4,6 +4,21 @@ angular.module('GoldRush')
 function nugCtrl(){
 
 	var nug = this;
+	// console.log('!');
+	nug.markers = []; // This will be our marker collection
 
-	console.log('!')
+	nug.createMarker = function(event){
+		// Accessing the event object
+		console.log(event);
+		var coordX = event.pageX;
+		var coordY = event.pageY;
+
+		// Create a new marker
+		var newMarker = {
+			x : coordX,
+			y : coordY
+		}
+		// Push into array of markers
+		nug.markers.push(newMarker);
+	}
 }

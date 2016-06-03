ff4371a0d35bc428841c3d27debe451183c37e18
devaio <robert.camp@outlook.com>
2016-06-03 15:45:09 -0600

    ER : Gold-Rush: Responsive... kinda

--- week3/exercise-goldrush/app.nuggetController.js
+++ week3/exercise-goldrush/app.nuggetController.js
@@ -7,18 +7,20 @@ function nugCtrl(){
 	// console.log('!');
 	nug.markers = []; // This will be our marker collection
 
+	// Triggered by our ngClick on the map
 	nug.createMarker = function(event){
 		// Accessing the event object
 		console.log(event);
-		var coordX = event.pageX;
-		var coordY = event.pageY;
-
+		var coordX = (event.pageX - 20) / document.body.clientWidth;
+		var coordY = (event.pageY - 20) / document.body.clientHeight;
 		// Create a new marker
 		var newMarker = {
-			x : coordX,
-			y : coordY
+			x : coordX * 100,
+			y : coordY * 100
 		}
 		// Push into array of markers
 		nug.markers.push(newMarker);
+
+		console.log(nug.markers)
 	}
 }

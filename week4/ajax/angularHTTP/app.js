983f1a4eb58856a0fb372c8b9ae88cb2d0324953
toneloke <david.guerrero87@live.com>
2016-06-08 11:25:41 -0600

    Finished Lecture

--- week4/ajax/angularHTTP/app.js
+++ week4/ajax/angularHTTP/app.js
@@ -9,15 +9,25 @@
       var pCtrl = this
       pCtrl.text = 'PokeApp'
 
-      pCtrl.searchPokedex = function(){
-        console.log('searching for pokemon....');
-        // $http.get()
+      pCtrl.searchPokedex = function(url){
+        console.log('searching for pokemon....')
+
+        $http.get(url)
+          .then(function(response){
+            console.log("Pokemon data:", response)
+            pCtrl.currentPokemon = response.data
+          }, function(error){
+            console.error(error)
+          })
+        // $http.get(url).then(success, error)
+        // function success(res){}
+        // function error(e){}
       }
 
       $http.get('http://pokeapi.co/api/v2/pokemon?limit=811')
         // .then(success, error)
         .then(function(response){
-          console.log(response)
+          console.log("Pokedex:", response)
           pCtrl.pokedex = response.data.results
         }, function(error){
           console.error(error)

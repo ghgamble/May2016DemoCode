938c4e6d6ae4681d057e667ed7881cb9b7c626d2
toneloke <david.guerrero87@live.com>
2016-06-08 13:53:07 -0600

    spotify added

--- week4/ajax/angularHTTP/app.js
+++ week4/ajax/angularHTTP/app.js
@@ -1,73 +1,87 @@
-(function() {
-  angular.module('pokeApp',[])
+;(function () {
+  angular.module('pokeApp', ['spotify'])
+    .config(function (SpotifyProvider) {
+      SpotifyProvider.setClientId('c79d6d2d5ac74b39a6048c4342d0fb7e')
+      SpotifyProvider.setRedirectUri('http://localhost:8080')
+      SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public')
+    // If you already have an auth token
+    // SpotifyProvider.setAuthToken('<AUTH_TOKEN>')
+    })
     // .controller('pokeController', ['$scope', '$log', function($log, $scope){}])
     .controller('pokeController', pokeCtrl)
-    .factory("MyYelpAPI", yelpApi)
+    .factory('MyYelpAPI', yelpApi)
 
-    pokeCtrl.$inject = ['$scope','$http','MyYelpAPI']
+  pokeCtrl.$inject = ['$scope', '$http', 'MyYelpAPI', 'Spotify']
 
-    function pokeCtrl($scope,$http, MyYelpAPI){
-      var pCtrl = this
-      pCtrl.text = 'PokeApp'
-      MyYelpAPI.retrieveYelp('', function(data) {
-                    console.log(data)
+  function pokeCtrl ($scope, $http, MyYelpAPI,Spotify) {
+    var pCtrl = this
+    pCtrl.text = 'PokeApp'
+    MyYelpAPI.retrieveYelp('', function (data) {
+      console.log(data)
 
-                })
-      pCtrl.searchPokedex = function(url){
-        console.log('searching for pokemon....')
+    })
+    Spotify.getAlbum('0sNOF9WDwhWunNAHPD3Baj').then(function (data) {
+      console.log("from Spotify",data)
+    })
+    pCtrl.searchPokedex = function (url) {
+      console.log('searching for pokemon....')
 
-        $http.get(url)
-          .then(function(response){
-            console.log("Pokemon data:", response)
-            pCtrl.currentPokemon = response.data
-          }, function(error){
-            console.error(error)
-          })
-        // $http.get(url).then(success, error)
-        // function success(res){}
-        // function error(e){}
-      }
-
-      $http.get('http://pokeapi.co/api/v2/pokemon?limit=811')
-        // .then(success, error)
-        .then(function(response){
-          console.log("Pokedex:", response)
-          pCtrl.pokedex = response.data.results
-        }, function(error){
+      $http.get(url)
+        .then(function (response) {
+          console.log('Pokemon data:', response)
+          pCtrl.currentPokemon = response.data
+        }, function (error) {
           console.error(error)
         })
-
+    // $http.get(url).then(success, error)
+    // function success(res){}
+    // function error(e){}
     }
 
-    //Yelp Factory for requests
-    function yelpApi($http){
-          return {
-              retrieveYelp: function(name, callback) {
-                  var method = 'GET';
-                  var url = 'http://api.yelp.com/v2/search';
-                  var params = {
-                          callback: 'angular.callbacks._0',
-                          location: 'San+Francisc',
-                          oauth_consumer_key: 'IQaizv7T1_qfhOvq3iurXQ', //Consumer Key
-                          oauth_token: 'SWfeNyhhWT85CLWryOpbtLMKD8hIsd8v', //Token
-                          oauth_signature_method: "HMAC-SHA1",
-                          oauth_timestamp: new Date().getTime(),
-                          oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-                          term: 'food'
-                      };
-                  var consumerSecret = '4uXV53TabsI-kSyul7ZBqcuP9bs'; //Consumer Secret
-                  var tokenSecret = '7yUzFe9aVz2fXFM20TigUZ5nrDU'; //Token Secret
-                  var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
-                  params['oauth_signature'] = signature;
-                  $http.jsonp(url, {params: params}).success(callback);
-              }
-          }
+    $http.get('http://pokeapi.co/api/v2/pokemon?limit=811')
+      // .then(success, error)
+      .then(function (response) {
+        console.log('Pokedex:', response)
+        pCtrl.pokedex = response.data.results
+      }, function (error) {
+        console.error(error)
+      })
+
+  }
+
+  // Yelp Factory for requests
+  function yelpApi ($http) {
+    return {
+      retrieveYelp: function (name, callback) {
+        var method = 'GET'
+        var url = 'http://api.yelp.com/v2/search'
+        var params = {
+          callback: 'angular.callbacks._0',
+          location: 'San+Francisc',
+          oauth_consumer_key: 'IQaizv7T1_qfhOvq3iurXQ', // Consumer Key
+          oauth_token: 'SWfeNyhhWT85CLWryOpbtLMKD8hIsd8v', // Token
+          oauth_signature_method: 'HMAC-SHA1',
+          oauth_timestamp: new Date().getTime(),
+          oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+          term: 'food'
+        }
+        var consumerSecret = '4uXV53TabsI-kSyul7ZBqcuP9bs' // Consumer Secret
+        var tokenSecret = '7yUzFe9aVz2fXFM20TigUZ5nrDU' // Token Secret
+        var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {
+          encodeSignature: false
+        })
+        params['oauth_signature'] = signature
+        $http.jsonp(url, {
+          params: params
+        }).success(callback)
+      }
     }
-    function randomString(length, chars) {
-                   var result = '';
-                   for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-                   return result;
-               }
+  }
 
+  function randomString (length, chars) {
+    var result = ''
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))]
+    return result
+  }
 
-}());
+}())

f056d312b723c0925b857d9508e1b4b0655da984
devaio <robert.camp@outlook.com>
2016-06-20 10:47:29 -0600

    Intro-Node : process / argv

--- week6/intro-node/helloworld.js
+++ week6/intro-node/helloworld.js
@@ -8,8 +8,15 @@
 
 // Requiring a module
 // Node has a handful of built in modules, one of those is called `fs`
-var fs = require('fs');
-// console.log(fs);
-console.log(
-    fs.readFileSync('./notes.txt', 'utf-8')
-);
+// var fs = require('fs');
+// // console.log(fs);
+// console.log(
+//     fs.readFileSync('./notes.txt', 'utf-8')
+// );
+
+// Arguments from command line
+// console.log(process.argv);
+
+// Process object
+// console.log(process.memoryUsage()); // Memory usage
+// console.log(process.env); // Stores environment variables

05dbfcbf574473f34e10b124859026182ed17b10
Brandon Jimenez <brandon@augur.io>
2016-06-23 17:16:17 -0600

    UPdates to lecture notes and examples

--- week6/req-object/server.js
+++ week6/req-object/server.js
@@ -11,14 +11,17 @@ var app = express()
 
 // Set up initial config / middleware mounting
 app.use( morgan('dev') )
-// app.use( bodyParser.urlencoded({ extended : true }) ) //
-// app.use( bodyParser.json() )
-app.use( bodyParser() )
+// @reference: https://www.npmjs.com/package/body-parser
+app.use( bodyParser.urlencoded({ extended : true }) )
+app.use( bodyParser.json() )
+// app.use( bodyParser() )
 app.use( express.static(path.resolve(__dirname, 'public')) )
 
 // Routes
-// GET :: http://localhost:8080/library/books
+// GET :: http://localhost:8080/library/books?title=Hamlet&limit=1
 app.get('/library/books', function( req, res ){
+    // JSON viewer extension
+    // @reference: https://chrome.google.com/webstore/detail/jsonview/chklaanhfefbnpoihckbnefhakgolnmc?hl=en
     var reqObjProps = {
         query: req.query,
         ip: req.ip,
@@ -33,28 +36,39 @@ app.get('/library/books', function( req, res ){
 })
 
 // POST :: http://localhost:8080/library/books
-// resource :: http://www.w3schools.com/tags/ref_urlencode.asp
+// @reference: http://www.w3schools.com/tags/ref_urlencode.asp
 app.post('/library/books', function( req, res ){
     console.log(req.body)
-    res.send(req.body)
+    // req.body.title = req.body.title.toUpperCase()
+    res.json(req.body)
 })
-
+// GET :: http://localhost:8080/library/books/1
 app.get('/library/book/:id', function( req, res ){
-    console.log(req.params)
-    res.send(req.params)
+    console.log('Params:', req.params.id) // 1
+    res.json(req.params.id) // setting JSON headers in the the response so that the browser definitely knows it is recieving JSON
 })
-
-app.get('/library/books/:year/:author', function( req, res ){
-    console.log(req.params)
-    res.send(req.params)
-})
-
+// POST :: http://localhost:8080/library/books/1
 app.post('/library/book/:id', function( req, res ){
-    console.log(req.params)
+    console.log('Params:', req.params)
     res.send({
         params: req.params,
         body: req.body
     })
+})
+
+// GET :: http://localhost:8080/library/search/books?title=Hamlet
+app.get('/library/search/:topic', function( req, res ){
+    console.log('Params:', req.params.id) // 1
+    res.json({
+        params: req.params,
+        query: req.query
+    }) // setting JSON headers in the the response so that the browser definitely knows it is recieving JSON
+})
+
+// GET :: http://localhost:8080/library/books/1589/shakespeare
+app.get('/library/books/:year/:author', function( req, res ){
+    console.log('Params:', req.params)
+    res.send(req.params)
 })
 
 // Listen for connections

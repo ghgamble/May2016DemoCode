4e0acc48138b0abcfa8689ba9ff75b1179dfd54d
Brandon Jimenez <brandon@augur.io>
2016-06-28 09:25:45 -0600

    Uploading lecture progress

---
+++ week7/mongoose-ajax/donuts/server.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var logger = require('morgan')
+var mongoose = require('mongoose')
+var apiRouter = require('./api_routes')
+var port = 8080
+
+var app = express()
+
+mongoose.connect('monogodb://localhost/donuts',
+    function( error ){
+        if( error ) {
+            console.error('ERROR starting mongoose!', error)
+        } else {
+            console.log('Mongoose connected successfully')
+        }
+    })
+
+app.use(bodyParser())
+app.use(logger('dev'))
+app.use('/api/v0', apiRouter)
+
+app.listen(port, function(error){
+    if( error ) {
+        console.error('ERROR starting server!', error)
+    } else {
+        console.log('Server started successfully on port:', port)
+    }
+})

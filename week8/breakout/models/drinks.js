c61e0394ad5251c97033f222c40cdd1f782ead98
devaio <robert.camp@outlook.com>
2016-07-06 10:07:13 -0600

    Set up Doc Reference in drinkSchema

--- week8/breakout/models/drinks.js
+++ week8/breakout/models/drinks.js
@@ -3,7 +3,15 @@ var mongoose = require('mongoose');
 var drinkSchema = mongoose.Schema({
 	name 				: String,
 	// Ingredients will come from another model
-	ingredients 		: Array,
+	ingredients 		: [
+		{
+			qty : Number,
+			ing : {
+				type : mongoose.Schema.ObjectId,
+				ref  : 'Ingredient'
+			}
+		}
+	],
 	shaken 				: Boolean,
 	wellDrink   		: Boolean,
 	price       		: Number,
